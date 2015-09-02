e539b08f3edafab2a0cc96b43ba570eeba3b244a
xiao <xiao@tokbox.com>
2015-09-02 12:29:57 -0700

    Search in Rotated Sorted Array

--- src/main/java/com/xelllee/code/leetcode/RemoveDuplicatesfromSortedArray2.java
+++ src/main/java/com/xelllee/code/leetcode/RemoveDuplicatesfromSortedArray2.java
@@ -1,21 +1,63 @@
 package com.xelllee.code.leetcode;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
- * Created by xiao on 9/2/15.
+ * Follow up for "Remove Duplicates":
+ * What if duplicates are allowed at most twice?
+ * <p/>
+ * For example,
+ * Given sorted array nums = [1,1,1,2,2,3],
+ * <p/>
+ * Your function should return length = 5, with the first five elements of nums being 1, 1, 2, 2 and 3.
+ * It doesn't matter what you leave beyond the new length.
  */
 public class RemoveDuplicatesfromSortedArray2 {
 
 
-
     public static void main(String[] args) {
+
+
+        int[] nums = {1, 1, 1, 2, 2, 3};
+//        int[] nums = {1, 2, 2, 3};
+
+        System.out.println(makeItRight(nums));
+
 
     }
 
 
-    public static String makeItRight() {
+    public static int makeItRight(int[] nums) {
+
+        if (nums == null || nums.length == 0) return 0;
+
+        int size = 1;
+        boolean dup = false;
 
 
-        return null;
+        for (int i = 1; i < nums.length; i++) {
+
+            if (!(nums[size - 1] == nums[i]) || !dup) {
+                dup = (nums[size - 1] == nums[i]);
+                size++;
+                nums[size - 1] = nums[i];
+            }
+//
+//            if (nums[size - 1] == nums[i]) {
+//                if (!dup) {
+//                    size++;
+//                    dup = true;
+//                }
+//            } else {
+//                size++;
+//                dup = false;
+//                nums[size - 1] = nums[i];
+//            }
+        }
+
+        System.out.println(Arrays.toString(nums));
+        return size;
 
     }
 
