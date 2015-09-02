76a84078437cea760a85f5dd963efcbd77c9eb34
xiao <xiao@tokbox.com>
2015-09-02 11:39:26 -0700

    first commit

---
+++ src/main/java/com/xelllee/code/leetcode/RemoveDuplicatesfromSortedArray.java
@@ -0,0 +1,48 @@
+package com.xelllee.code.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * Given a sorted array, remove the duplicates in place such that each element appear only once and return the new length.
+ * <p/>
+ * Do not allocate extra space for another array, you must do this in place with constant memory.
+ * <p/>
+ * For example,
+ * Given input array nums = [1,1,2],
+ * <p/>
+ * Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+ * It doesn't matter what you leave beyond the new length.
+ */
+public class RemoveDuplicatesfromSortedArray {
+
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 2, 3, 5, 6, 6, 9, 9, 19};
+        System.out.println(removeDuplicates(nums));
+
+    }
+
+
+    public static int removeDuplicates(int[] arr) {
+
+        if (arr == null || arr.length < 1) return 0;
+
+        int size = 1;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (!(arr[size - 1] == arr[i])) {
+                size++;
+                arr[size - 1] = arr[i];
+            }
+        }
+
+//        System.out.println(size);
+
+        return size;
+
+    }
+
+
+}
