f682f3e19a98092e0922235953438761c776dda0
xiao <xiao@tokbox.com>
2015-09-08 20:21:27 -0700

    array si done

---
+++ src/main/java/com/xelllee/code/leetcode/GrayCode.java
@@ -0,0 +1,40 @@
+package com.xelllee.code.leetcode;
+
+import java.util.List;
+
+public class GrayCode {
+
+    /*
+    *
+    The gray code is a binary numeral system where two successive values differ in only one bit.
+    Given a non-negative integer n representing the total number of bits in the code, print the sequence of gray code.
+    A gray code sequence must begin with 0.
+    For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+
+    00 - 0
+    01 - 1
+    11 - 3
+    10 - 2
+
+    Note:
+    For a given n, a gray code sequence is not uniquely defined.
+    For example, [0,2,3,1] is also a valid gray code sequence according to the above definition.
+    For now, the judge is able to judge based on one instance of gray code sequence. Sorry about that.
+
+*/
+
+
+    //TODO: not interesting
+    public static void main(String[] args) {
+
+
+    }
+
+
+    public static List<Integer> makeItRight(int n) {
+
+        return null;
+
+    }
+
+}
---
+++ src/main/java/com/xelllee/code/leetcode/SetMatrixZeroes.java
@@ -0,0 +1,84 @@
+package com.xelllee.code.leetcode;
+
+import java.util.Arrays;
+
+public class SetMatrixZeroes {
+
+
+//    Given a m x n matrix, if an element is 0, set its entire row and column to 0. Do it in place.
+//    click to show follow up.
+
+
+//    Follow up:
+//    Did you use extra space?
+//    A straight forward solution using O(mn) space is probably a bad idea.
+//    A simple improvement uses O(m + n) space, but still not the best solution.
+//    Could you devise a constant space solution?
+
+    public static void main(String[] args) {
+
+        int[][] matrix = {{1, 1, 0},
+                {2, 0, 2},
+                {1, 2, 2}};
+
+        makeItRight(matrix);
+
+
+    }
+
+
+    //clumzy code
+    public static void makeItRight(int[][] matrix) {
+
+
+        if (matrix == null) return;
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            int m = matrix.length;
+            int n = matrix[i].length;
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[i][j] == 0) continue;
+
+                boolean flag = false;
+
+                for (int t = 0; t < m; t++) {
+                    if (matrix[t][j] == 0) {
+                        flag = true;
+                    }
+                }
+
+                if (!flag) {
+                    for (int t = 0; t < n; t++) {
+                        if (matrix[i][t] == 0) {
+                            flag = true;
+                        }
+                    }
+                }
+
+                if (flag) {
+                    matrix[i][j] = -1;
+                } else {
+                    matrix[i][j] = (matrix[i][j] > 0) ? matrix[i][j] : (matrix[i][j] != Integer.MIN_VALUE) ? matrix[i][j] - 1 : matrix[i][j];
+                }
+
+            }
+        }
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < 0 && matrix[i][j] != Integer.MIN_VALUE) matrix[i][j] = matrix[i][j] + 1;
+            }
+        }
+
+
+        for (int[] arr : matrix) {
+            System.out.print(Arrays.toString(arr));
+        }
+
+    }
+
+
+}
